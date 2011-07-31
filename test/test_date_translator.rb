e76c528babaae7b9cd941a87bca4ad299d87f1de
Andrew Vos <andrew.vos@gmail.com>
2011-07-31 19:18:58 +0100

    removed the pluralize tests because they were just taking up too much space

--- test/test_date_translator.rb
+++ test/test_date_translator.rb
@@ -24,53 +24,34 @@ class DateTranslatorTest < MiniTest::Unit::TestCase
     assert_equal "now", translate("2011-10-23T00:00:10")
   end
 
-  def test_translate_returns_years_if_time_difference_is_more_than_a_year
+  def test_translate_returns_years
     assert_equal "1 year", translate("2012-10-23T00:00:00")
-  end
-
-  def test_translate_pluralizes_years_if_time_difference_is_two_years_or_more
     assert_equal "2 years", translate("2013-10-23T00:00:00")
   end
 
-  def test_translate_returns_months_if_time_difference_is_more_than_one_month
+  def test_translate_returns_months
     assert_equal "1 month", translate("2011-11-23T00:00:00")
-  end
-
-  def test_translate_pluralizes_months_if_time_difference_is_two_months_or_more
     assert_equal "2 months", translate("2011-12-23T00:00:00")
   end
 
-  def test_translate_returns_days_if_time_difference_is_more_than_a_day
+  def test_translate_returns_days
     assert_equal "1 day", translate("2011-10-24T00:00:00")
-  end
-
-  def test_translate_pluralizes_days_if_time_difference_is_two_days_or_more
     assert_equal "2 days", translate("2011-10-25T00:00:00")
   end
 
-  def test_translate_returns_hours_if_time_difference_is_more_than_an_hour
+  def test_translate_returns_hours
     assert_equal "1 hour", translate("2011-10-23T01:00:00")
-  end
-
-  def test_translate_pluralizes_hours_if_time_difference_is_two_hours_or_more
     assert_equal "2 hours", translate("2011-10-23T02:00:00")
   end
 
-  def test_translate_returns_minutes_if_time_difference_is_more_than_a_minute
+  def test_translate_returns_minutes
     assert_equal "1 minute", translate("2011-10-23T00:01:00")
-  end
-
-  def test_translate_pluralizes_minutes_if_time_difference_is_two_minutes_or_more
     assert_equal "2 minutes", translate("2011-10-23T00:02:00")
   end
 
-  def test_translate_returns_one_second
+  def test_translate_returns_seconds
     @date_translator.now_tolerance = 0
     assert_equal "1 second", translate("2011-10-23T00:00:01")
-  end
-
-  def test_translate_pluralizes_seconds
-    @date_translator.now_tolerance = 0
     assert_equal "2 seconds", translate("2011-10-23T00:00:02")
   end
 
