69bff09bc8eabc05a4d1fe4642e6ae1cedce7fb2
Andrew Vos <andrew.vos@gmail.com>
2011-07-31 18:44:54 +0100

    got most of the translation done

--- lib/human-date/date_translator.rb
+++ lib/human-date/date_translator.rb
@@ -4,13 +4,40 @@ module HumanDate
   class DateTranslator
     attr_accessor :now_tolerance
 
+    ONE_YEAR    = 365 * 24 * 60 * 60
+    ONE_MONTH   = 30 * 24 * 60 * 60
+    ONE_DAY     = 24 * 60 * 60
+    ONE_HOUR    = 60 * 60
+    ONE_MINUTE  = 60
+
     def initialize
       @now_tolerance = 10
     end
 
     def translate(from, to)
-      total_difference_in_seconds = ((to-from) *60 * 60*24).to_i
+      total_difference_in_seconds = ((to-from) * 60 * 60 * 24).to_i
       return 'now' if total_difference_in_seconds.abs <= now_tolerance
+
+      if total_difference_in_seconds >= ONE_YEAR
+        result = "#{total_difference_in_seconds / ONE_YEAR} year"
+        result += 's' if total_difference_in_seconds >= (ONE_YEAR * 2)
+      elsif total_difference_in_seconds >= ONE_MONTH
+        result = "#{total_difference_in_seconds / ONE_MONTH} month"
+        result += 's' if total_difference_in_seconds >= (ONE_MONTH * 2)
+      elsif total_difference_in_seconds >= ONE_DAY
+        result = "#{total_difference_in_seconds / ONE_DAY} day"
+        result += 's' if total_difference_in_seconds >= (ONE_DAY * 2)
+      elsif total_difference_in_seconds >= ONE_HOUR
+        result = "#{total_difference_in_seconds / ONE_HOUR} hour"
+        result += 's' if total_difference_in_seconds >= (ONE_HOUR * 2)
+      elsif total_difference_in_seconds >= ONE_MINUTE
+        result = "#{total_difference_in_seconds / ONE_MINUTE} minute"
+        result += 's' if total_difference_in_seconds >= (ONE_MINUTE * 2)
+      else
+        result = "#{total_difference_in_seconds} second"
+      end
+      return result
     end
+
   end
 end
--- test/test_date_translator.rb
+++ test/test_date_translator.rb
@@ -1,3 +1,5 @@
+require 'minitest/pride'
+require 'minitest/unit'
 require 'minitest/autorun'
 require_relative '../lib/human-date'
 
@@ -6,26 +8,92 @@ class DateTranslatorTest < MiniTest::Unit::TestCase
     @date_translator = HumanDate::DateTranslator.new
   end
 
-  def test_that_translate_returns_now_if_dates_are_equal
+  def setup_time from, to
+    @from = DateTime.parse(from)
+    @to   = DateTime.parse(to)
+  end
+
+  def test_translate_returns_now_if_dates_are_equal
     now = DateTime.now
     assert_equal 'now', @date_translator.translate(now, now)
   end
 
-  def test_that_translate_returns_now_if_date_difference_is_within_tolerance
-    from  = DateTime.parse("2011-10-23T00:00:10")
-    to    = DateTime.parse("2011-10-23T00:00:20")
+  def test_translate_returns_now_if_date_difference_is_within_tolerance
+    setup_time "2011-10-23T00:00:10", "2011-10-23T00:00:20"
 
     @date_translator.now_tolerance = 15
-    assert_equal 'now', @date_translator.translate(from, to)
-    assert_equal 'now', @date_translator.translate(to, from)
+    assert_equal 'now', @date_translator.translate(@from, @to)
+    assert_equal 'now', @date_translator.translate(@to, @from)
 
     @date_translator.now_tolerance = 10
-    assert_equal 'now', @date_translator.translate(from, to)
-    assert_equal 'now', @date_translator.translate(to, from)
+    assert_equal 'now', @date_translator.translate(@from, @to)
+    assert_equal 'now', @date_translator.translate(@to, @from)
 
     @date_translator.now_tolerance = 9
-    refute_equal 'now', @date_translator.translate(from, to)
-    refute_equal 'now', @date_translator.translate(to, from)
+    refute_equal 'now', @date_translator.translate(@from, @to)
+    refute_equal 'now', @date_translator.translate(@to, @from)
+  end
+
+  def test_translate_returns_years_if_time_difference_is_more_than_a_year
+    setup_time "2011-10-23T00:00:00", "2012-10-23T00:00:00"
+    assert_equal '1 year', @date_translator.translate(@from, @to)
+  end
+
+  def test_translate_pluralizes_years_if_time_difference_is_two_years_or_more
+    setup_time "2011-10-23T00:00:00", "2013-10-23T00:00:00"
+    assert_equal '2 years', @date_translator.translate(@from, @to)
+  end
+
+  def test_translate_returns_months_if_time_difference_is_more_than_one_month
+    setup_time "2011-10-23T00:00:00", "2011-11-23T00:00:00"
+    assert_equal '1 month', @date_translator.translate(@from, @to)
+  end
+
+  def test_translate_pluralizes_months_if_time_difference_is_two_months_or_more
+    setup_time "2011-10-23T00:00:00", "2011-12-23T00:00:00"
+    assert_equal '2 months', @date_translator.translate(@from, @to)
+  end
+
+  def test_translate_returns_days_if_time_difference_is_more_than_a_day
+    setup_time "2011-10-23T00:00:00", "2011-10-24T00:00:00"
+    assert_equal '1 day', @date_translator.translate(@from, @to)
+  end
+
+  def test_translate_pluralizes_days_if_time_difference_is_two_days_or_more
+    setup_time "2011-10-23T00:00:00", "2011-10-25T00:00:00"
+    assert_equal '2 days', @date_translator.translate(@from, @to)
+  end
+
+  def test_translate_returns_hours_if_time_difference_is_more_than_an_hour
+    setup_time "2011-10-23T00:00:00", "2011-10-23T01:00:00"
+    assert_equal '1 hour', @date_translator.translate(@from, @to)
+  end
+
+  def test_translate_pluralizes_hours_if_time_difference_is_two_hours_or_more
+    setup_time "2011-10-23T00:00:00", "2011-10-23T02:00:00"
+    assert_equal '2 hours', @date_translator.translate(@from, @to)
+  end
+
+  def test_translate_returns_minutes_if_time_difference_is_more_than_a_minute
+    setup_time "2011-10-23T00:00:00", "2011-10-23T00:01:00"
+    assert_equal '1 minute', @date_translator.translate(@from, @to)
+  end
+
+  def test_translate_pluralizes_minutes_if_time_difference_is_two_minutes_or_more
+    setup_time "2011-10-23T00:00:00", "2011-10-23T00:02:00"
+    assert_equal '2 minutes', @date_translator.translate(@from, @to)
+  end
+
+  def test_translate_returns_one_second
+    @date_translator.now_tolerance = 0
+    setup_time "2011-10-23T00:00:00", "2011-10-23T00:00:01"
+    assert_equal '1 second', @date_translator.translate(@from, @to)
+  end
+
+  def test_translate_pluralizes_seconds
+    @date_translator.now_tolerance = 0
+    setup_time "2011-10-23T00:00:00", "2011-10-23T00:00:02"
+    assert_equal '2 seconds', @date_translator.translate(@from, @to)
   end
 
 end
