30ff0b924f629cd60aea28fca4282bb709d4d5e6
Andrew Vos <andrew.vos@gmail.com>
2011-07-31 17:59:24 +0100

    refactoring

--- lib/human-date/date_translator.rb
+++ lib/human-date/date_translator.rb
@@ -4,8 +4,11 @@ module HumanDate
   class DateTranslator
     attr_accessor :now_tolerance
 
+    def initialize
+      @now_tolerance = 10
+    end
+
     def translate(from, to)
-      return 'now' if from == to
       total_difference_in_seconds = ((to-from) *60 * 60*24).to_i
       return 'now' if total_difference_in_seconds.abs <= now_tolerance
     end
